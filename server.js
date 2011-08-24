6202283ed8802aad4b142474388ca75d7465faad
crahles <christoph@rahles.de>
2011-08-24 12:44:17 +0200

    added node-streamlogger

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require.paths.unshift(__dirname + "/vendor");
+require.paths.unshift(__dirname + "/vendor/node-streamlogger/lib");
+
+var
+  streamLogger = require('streamlogger'),
+  logger = new streamLogger.StreamLogger('server.log');
+  //Defaults to info, debug messages will not be logged at info
+  logger.level = logger.levels.debug;
 
 process.addListener('uncaughtException', function (err, stack) {
-  console.log('------------------------');
-  console.log('Exception: ' + err);
-  console.log(err.stack);
-  console.log('------------------------');
+  logger.debug('------------------------');
+  logger.debug('Exception: ' + err);
+  logger.debug(err.stack);
+  logger.debug('------------------------');
 });
 
 var LiveStats = require('./lib/livestats');
